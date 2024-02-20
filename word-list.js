5ca9d8998c982cccab42c027872a5e01aaa4abcf
isaumya14 <saumyasingh1495@gmail.com>
2024-02-20 16:17:19 +0530

    intital commit

---
+++ word-list.js
@@ -0,0 +1,57 @@
+const wordList=[
+  {
+    word: "guitar",
+    hint: "A musical instrument with strings."
+  },
+  {
+    word: "mountain",
+    hint: "A large natural elevation of the Earth's surface."
+  },
+  {
+    word: "astronomy",
+    hint: "The scientific study of celestial objects and phenomena."
+  },
+  {
+    word: "garden",
+    hint: "A place where plants, flowers, and vegetables grow."
+  },
+  {
+    word: "clouds",
+    hint: "They float in the sky and can bring rain."
+  },
+  {
+    word: "spring",
+    hint: "This season brings blooming flowers and warmer weather."
+  },
+  {
+    word: "warmth",
+    hint: "The quality of being comfortably heated."
+  },
+  {
+    word: "fields",
+    hint: "Open expanses of land, often used for agriculture."
+  },
+  {
+    word: "shades",
+    hint: "They block sunlight and provide relief on hot days."
+  },
+  {
+    word: "flowry",
+    hint: "Full of blossoms and petals."
+  },
+  {
+    word: "bright",
+    hint: "Shining with intense light."
+  },
+  {
+    word: "gently",
+    hint: "In a soft and mild manner."
+  },
+  {
+    word: "sunset",
+    hint: "The time when the sun dips below the horizon."
+  }, {
+    word: "saumya",
+    hint: "your game owner(cutieee me)"
+  }
+]
